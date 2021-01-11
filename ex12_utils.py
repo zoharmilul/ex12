0890a6ec3f8fc9759c39b31088c7f914e079d4a4
zoharmilul <8qxddflhxbw>
2021-01-11 20:28:41 +0200

    asdasd

--- ex12_utils.py
+++ ex12_utils.py
@@ -15,8 +15,6 @@ def is_valid_path(board, path, words):
             return
         if i != len(path) - 1 and check_valid_step(path[i], path[i + 1]):
             continue
-        else:
-            return
 
     word = _get_word(board, path)
     if word in words:
@@ -25,35 +23,37 @@ def is_valid_path(board, path, words):
 
 def find_length_n_words(n, board, words):
     valid_path = []
+    words_list = set(filter(lambda x: len(x) == n, words.keys()))
     for i in range(len(board)):
         for j in range(len(board[0])):
-            valid_path += _find_length_helepr(n, [(i, j)], [], board, words)
-
-    print(valid_path)
-
-def _find_length_helepr(n, curr_path, pathes, board, words):
-    if len(curr_path) == n:
-        pathes.append((_get_word(board,curr_path), curr_path[:]))
-        return curr_path
-    for step in get_valid_steps(curr_path[-1]):
-        if check_curr_path(n, curr_path + [step], board, words):
-            _find_length_helepr(n, curr_path + [step], pathes, board, words)
-
-    return pathes
+            valid_path += _find_length_helepr(n, [(i, j)], board[i][j], [], board, words_list)
+    return valid_path
 
 
-def check_curr_path(n, curr_path, board, words):
-    words_list = list(filter(lambda x: len(x) == n, list(words.keys())))
-    semi_word = _get_word(board, curr_path)
-    if semi_word:
+def _find_length_helepr(n, curr_path, curr_word, paths, board, words_list):
+    if len(curr_word) > n:
+        return
+    if len(curr_word) == n:
+        if curr_word and check_curr_word(curr_word,words_list):
+            paths.append((curr_word, curr_path))
+        return paths
+
+    if check_curr_word(curr_word, words_list):
+        for step in get_valid_steps(curr_path[-1], curr_path):
+            _find_length_helepr(n, curr_path + [step], curr_word + board[step[0]][step[1]], paths, board, words_list)
+    return paths
+
+
+def check_curr_word(curr_word, words_list):
+    if curr_word:
         for word in words_list:
-            if semi_word == word[:len(semi_word)]:
+            if word.startswith(curr_word):
                 return True
     return False
 
 
 def check_valid_coords(coord, path):
-    return 0 <= coord[0] <= 3 and 0 <= coord[1] <= 3 and path.count(coord) == 1
+    return 0 <= coord[0] <= 3 and 0 <= coord[1] <= 3 and path.count(coord) <= 1
 
 
 def check_valid_step(coord1, coord2):
@@ -68,20 +68,25 @@ def _get_word(board, path):
         return word
 
 
-def get_valid_steps(cord):
+def get_valid_steps(cord, path):
     steps = [(cord[0] + 1, cord[1]), (cord[0] + 1, cord[1] + 1), (cord[0] + 1, cord[1] -1),
                    (cord[0] - 1, cord[1]), (cord[0] - 1, cord[1] + 1), (cord[0] - 1, cord[1] -1),
                    (cord[0], cord[1] + 1), (cord[0], cord[1] - 1)]
-
-    valid_steps = list(filter(lambda x: check_valid_coords(x, [x]), steps))
+    valid_steps = filter(lambda x: check_valid_coords(x, path), steps)
     return valid_steps
 
-board = helper.randomize_board()
-words = load_words_dict("boggle_dict.txt")
-start = time.time()
-find_length_n_words(3, board, words)
-end = time.time()
-print(end-start)
+if __name__ == "__main__":
+    board = helper.randomize_board()
+    words = load_words_dict("boggle_dict.txt")
+    for i in range(3,17):
+        print(i)
+        start = time.time()
+        print(find_length_n_words(i, board, words))
+        end = time.time()
+        print(end - start)
+
+
+
 
 # if check_curr_path(n,curr_path,board, words):
 #     _find_length_helepr(n, curr_path + [(curr_path[-1][0], curr_path[-1][1] + 1)], pathes,board, words)
